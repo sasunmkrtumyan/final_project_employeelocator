ef570f433487f854dc8d2a4f8a995e0b2b7d6ded
Sasun MKrtumyan <sasunmkrtumyan92@gmail.com>
2021-08-27 13:16:50 +0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AboutPage from "./pages/aboutPage/AboutPage";
 import ContactPage from "./pages/contactPage/ContactPage";
